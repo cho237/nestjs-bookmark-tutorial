572753e75fcc2cbb2d98e747f48418ddb94f63ea
Kamix <kinslycho@gmail.com>
2024-09-20 11:24:44 +0100

    initial commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class AuthService {
+
+    signup(){
+        return {message: "I am signup"};
+    }
+
+    signin(){
+        return {message: "I am signin"};
+    }
+
+}
