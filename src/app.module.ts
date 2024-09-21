f82f8a9aacd382313522358ece8f5e802d0b09de
Kamix <kinslycho@gmail.com>
2024-09-21 09:09:34 +0100

    done with authentification

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AppModule, UserModule, BookmarkModule, AuthModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AppModule,
+    UserModule,
+    BookmarkModule,
+    AuthModule,
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
